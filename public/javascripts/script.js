c9d0faeccf0e34a26a121fe7ec9d1189c5b29c50
Alex Gomez <108309608+Alex-Apex@users.noreply.github.com>
2023-11-11 13:45:15 +0000

    BugFix: corrected the transition animation for each interaction with the AI. Now, they fade in and out

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -16,19 +16,4 @@ document.getElementById('user-input').addEventListener('input', function() {
     sendButton.disabled = textAreaContent.length === 0;
   });
 
-  document.body.addEventListener('htmx:beforeSwap', function(event) {
-    if (event.target.id === 'ai-response') {
-      event.detail.target.classList.remove('hx-fade-in');
-      event.detail.target.classList.add('hx-fade-out');
-      return false; // Prevents the default swap
-    }
-  });
-
-  document.body.addEventListener('htmx:afterSwap', function(event) {
-    if (event.target.id === 'ai-response') {
-      event.detail.target.classList.remove('hx-fade-out');
-      setTimeout(function() {
-        event.detail.target.classList.add('hx-fade-in');
-      }, 10); // Timeout to allow fade-out to complete
-    }
-  });
+  
